4d37e97fd06fcb5f9af99c5b900c90581e48abd6
Aleksandar <aleksandar.garov@novarto.com>
2016-04-30 16:19:53 +0300

    added routing for login and register

--- app/js/app.js
+++ app/js/app.js
@@ -13,6 +13,16 @@ app.config(['$routeProvider', function($routeProvider) {
       templateUrl: 'templates/home.html',
       controller: 'HomeController'
     });
+
+    $routeProvider.when('/login', {
+        templateUrl: 'templates/login.html',
+        controller: 'LoginController'
+    });
+
+    $routeProvider.when('/register', {
+        templateUrl: 'templates/register.html',
+        controller: 'RegisterController'
+    });
   
     $routeProvider.otherwise({redirectTo: '/'});
 }]);
